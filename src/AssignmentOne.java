cfc010f4179b8da0caf5ccb4067bac5544bf6381
EpiphanyOct <144693830+EpiphanyOct@users.noreply.github.com>
2024-11-20 05:01:39 +0800

    Add comment
    Add comment

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,3 +1,4 @@
+//Qingyang Jiao  24516659
 public class AssignmentOne {
     public static void main(String[] args) {
         GeneralPractitioner gp = new GeneralPractitioner(1, "Dr. John", "General Medicine", true);
